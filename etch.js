5927ba551eaee6304e9ef7bf6aead8637f5446cc
1256639 <167916944+1256639@users.noreply.github.com>
2025-06-25 12:03:04 -0400

    Hover effect added

--- etch.js
+++ etch.js
@@ -3,5 +3,10 @@ const container = document.getElementById('container');
 for (let i = 0; i < 16 * 16; i++){
     const square = document.createElement('div');
     square.classList.add('square');
+
+    square.addEventListener('mouseover', function(){
+        square.style.backgroundColor = '#222';
+    });
+
     container.appendChild(square);
 }
