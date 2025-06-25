f693aa30966a20b20c08d7cd3b30bc93a50185cb
1256639 <167916944+1256639@users.noreply.github.com>
2025-06-25 12:29:43 -0400

    Resize option added

--- etch.js
+++ etch.js
@@ -1,12 +1,38 @@
 const container = document.getElementById('container');
+const resizeBtn = document.getElementById('resizeBtn');
+const CONTAINER_SIZE = 480;
 
-for (let i = 0; i < 16 * 16; i++){
-    const square = document.createElement('div');
-    square.classList.add('square');
+function makeGrid(size){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
 
-    square.addEventListener('mouseover', function(){
-        square.style.backgroundColor = '#222';
-    });
+    container.style.display = 'flex';
+    container.style.flexWrap = 'wrap';
+    
+    const squareSize = CONTAINER_SIZE / size;
 
-    container.appendChild(square);
+    for (let i = 0; i < size * size; i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = squareSize + "px";
+        square.style.height = squareSize + "px";
+        square.addEventListener('mouseover', function(){
+            square.style.backgroundColor = '#222';
+        });
+        container.appendChild(square);
+    }
 }
+
+makeGrid(16);
+
+resizeBtn.addEventListener('click', function(){
+    let newSize = prompt("Enter a size between 1 and 100:", 16);
+    newSize = Number(newSize);
+
+    if (newSize >= 1 && newSize <= 100){
+        makeGrid(newSize);
+    } else {
+        alert("Please enter a number between 1 and 100.")
+    }
+});
